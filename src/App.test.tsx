849acd0f5793e1204d22f9518d92bfc51a64bda8
Davis Sorenson <davis.sorenson@gmail.com>
2020-03-20 20:08:53 +0200

    More tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,29 @@
 import React from "react"
-import { render, waitForElement } from "@testing-library/react"
+import { render, waitForElement, fireEvent } from "@testing-library/react"
 import App from "./App"
 
 test("loads cat facts", async () => {
   const { getByText } = render(<App deterministic={true} />)
-  const factLoaderButton = getByText(/load facts/i)
-  factLoaderButton.click()
+  const factLoaderButton = getByText(/load more facts/i)
 
+  fireEvent.click(factLoaderButton, { button: 1 })
   const firstFact = await waitForElement(() =>
     getByText(/By the time a cat is 9 years old/i)
   )
 
   expect(firstFact).toBeInTheDocument()
+})
+
+test("toggles mute correctly", async () => {
+  const { getByText } = render(<App />)
+  const muteButton = getByText(/volume_up/i)
+
+  expect(muteButton).toBeInTheDocument()
+  fireEvent.click(muteButton, { button: 1 })
+
+  const unmuteButton = getByText(/volume_off/i)
+  expect(unmuteButton).toBeInTheDocument()
+
+  fireEvent.click(unmuteButton, { button: 1 })
+  expect(muteButton).toBeInTheDocument()
 })
